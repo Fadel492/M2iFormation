daf05216a53efec05f25cd7e355b21e776d9b84a
fadel Diakhate <fadeldiakhate95@gmail.com>
2022-01-06 20:43:50 +0100

    Suite exo poo

--- Exercice/main.py
+++ Exercice/main.py
@@ -1,5 +1,8 @@
+#from _typeshed import Self
+import sys
 import numpy as np
-"""
+
+""" 
 Ecrire une classe qui permet de représenter un point dans un plan (abscisse, ordonnée)
 - Ecrire le constructeur
 - Ecrire les getters / setters
@@ -18,7 +21,7 @@ class point:
 
 
     def __init__(self,abcisse,ordonne):
-        print("creation d'un point.......")
+        #print("creation d'un point.......")
         self._X=abcisse
         self._Y=ordonne
 
@@ -35,20 +38,107 @@ class point:
         self._Y=newordonne
 
     def AdditionPoint(A,B):
-        pass
+
+        absf=A._X - B._X
+        ordf=A._Y - B._Y
+        return point(absf,ordf)
+
+
         
 
-    def distance(pdebut,pfin):
-        return np.sqrt(np.pow(pfin.y-pdebut.y,2)+np.pow(pfin.x-pdebut.x,2))
+    def distance(P1,P2):
+        resultat=np.sqrt((P2._X - P1._Y)**2+(P2._Y - P1._X)**2)
+        return resultat
 
+    def Milieu(P1,P2):
+        return ((P1._X + P2._X)/2,(P1._Y + P2._Y)/2)
+        
 
-print("Lancement du programme...")
+print("Lancement du programme1...")
 
 A1=point(0,2)
 B1=point(1,3)
+
 print(f"Creation du premier point A1 d'abcisse {A1._X} et d'ordonnée {A1._Y}")
-print(f"Creation du premier point A1 d'abcisse {B1._X} et d'ordonnée {B1._Y}")
+print("****************************************************************************")
+print(f"Creation du premier point B1 d'abcisse {B1._X} et d'ordonnée {B1._Y}")
+
+d=point.distance(A1,B1)
+
+M=point.Milieu(A1,B1)
+print("****************************************************************************")
+print(f"La distance entre le point A et B de coordonnée respectives A({A1._X} , {A1._Y}) et  B({B1._X} , {B1._Y}) est {d}cm")
+print("****************************************************************************")
+print(f"Le milieu entre le point A et B de coordonnée respectives A({A1._X} , {A1._Y}) et B({B1._X} , {B1._Y}) est {M}")
+
+print("*************************** Fin du programme ***************************************************************")
+
+""" 
+Ecrire une classe compte bancaire qui a pour attribut un numero de compte, un nom de client, le solde, 
+le nombre d’opérations réalisées et une variable de classe nb max d’opérations
+- Ecrire le constructeur du compte
+- Ecrire les getters
+- Ecrire la représentation graphique sous la forme « nom : xxx, numCpte : yyy, solde : 100, nbreOp : 3 »
+- Ecrire une méthode versement qui gère les versements
+- Ecrire une méthode retrait qui gère les retraits (notamment s’assure que le compte est suffisamment provisionné)
+- Ecrire une méthode virement qui permet d’envoyer de l’argent en renseignant le numéro de compte de destination
+- Ecrire une méthode frais qui applique des frais à hauteur de 1% de l’opération pour chaque opération 
+  en plus du nb max d’opérations par compte ➔ Les frais s’appliquent à tous type d’opération hormis l’affichage du compte
+"""
+
+class CompteBancaire:
+    NbmaxOp=0
+
+    def __init__(self,Numcomp,Nomclient,solde,Nboprealise):
+
+        self._Numcomp=Numcomp
+        self._Nomclient=Nomclient
+        self._solde=solde
+        self._Nboprealise=Nboprealise
+    
+    def _getNumcomp(self):
+        return self._Numcomp
+
+    def _getNomclien(self):
+        return self._Nomclient
+
+    def _getsolde(self):
+        return self._solde
+
+    def _getNboprealise(self):
+        return self._Nboprealise
+        
+
+    def Versement(self,soldeVersement,idVersement):
+
+        print(f"Versement de {soldeVersement} euro en cours sur ce compte compte ...")
+        if(self._Numcomp==idVersement):
+            self._solde=self._solde+soldeVersement
+        else:
+            print("le numero de compte saisie est invalide")
+        return self._solde
+
+    def Retrait(self,soldeRetrait):
+        if(self._solde <=soldeRetrait):
+            print("La somme que vous voulais retiré est superieur à votre solde")
+            print("Veuillez choisir un montant plus petite")
+        else:
+            print(f"vous avez retire {soldeRetrait} et il vous reste {self._solde-soldeRetrait} dans votre compte")
+        
+    def Virement(self,idvirement,soldeVirement):
+        print(f"Virement de {soldeVirement} euro sur IBAN {idvirement} ...")
 
 
-   
 
+print("Lancement du programme2...") 
+print("****************************************************************************")  
+Compte_Fadel=CompteBancaire(123,"DIAKHATE",100,3)
+print(f"Les information de votre compte est nom: {Compte_Fadel._Nomclient}, le solde:{Compte_Fadel._solde} euro et numero de compte:{Compte_Fadel._Numcomp}")
+
+print("****************************************************************************")
+Opversement=Compte_Fadel.Versement(100,123)
+print(f"Votre nouvau solde est de {Opversement} euro")
+print("****************************************************************************")
+Opretrait=Compte_Fadel.Retrait(50)
+print("****************************************************************************")
+opVirement=Compte_Fadel.Virement(124,100)
